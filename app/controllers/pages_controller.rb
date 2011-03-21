e1910e698f4221cbcde110f6fb9f488d0c683381
Sayem Islam <mail@sayemislam.com>
2011-03-21 17:03:17 -0500

    3.21

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,23 @@
 class PagesController < ApplicationController
   def home
-    @user = User.new
   end
 
   def register
+    @user = User.new
+    respond_to do | format |
+      format.html
+      format.xml { render :xml => @user }
+    end
   end
 
   def login
+    @user = User.new
+    respond_to do | format |
+      format.html
+      format.xml { render :xml => @user }
+    end
   end
 
   def account
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110319235146) do
+ActiveRecord::Schema.define(:version => 20110321183223) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.string   "hashed_password"
     t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
