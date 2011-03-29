0612da2de9b7bb547f104631078b717e64c99ee5
Sayem Islam <mail@sayemislam.com>
2011-03-29 14:59:32 -0500

    welcome email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
+      UserMailer.welcome_email(@user).deliver
     else
       render 'new'
     end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ActionMailer::Base
+  default :from => "admin@crunchalert.com"
+
+  def welcome_email(user)
+    @user = user
+    @url  = "http://crunchalert.com/login"
+    mail(:to => user.email,
+         :subject => "Welcome to CrunchAlert")
+  end
+end
