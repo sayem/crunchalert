e323e93840f0bd8b3b64c80d25915f19d133c189
Sayem Islam <mail@sayemislam.com>
2011-05-01 11:47:50 -0400

    user.rb update

--- crunchalert.rb
+++ crunchalert.rb
@@ -1,18 +1,32 @@
 require 'nokogiri'
 require 'open-uri'
+require 'date'
 
 begin
-  doc = Nokogiri::HTML(open('http://www.crunchbase.com/company/facebook'))
+  doc = Nokogiri::HTML(open('http://www.crunchbase.com/company/ideeli'))
   milestones = doc.css('#milestones li').each do |milestone|
+
+    yesterday = Date.today.prev_day.strftime("%m/%d/%y")
+    yesterday.slice!(0) if yesterday.slice(0) == '0'
+
+
+
+    if milestone.text =~ /#{yesterday}/
+      text = milestone.at_css('.milestone_text').to_s().gsub(/\/company\/ideeli/,'http://crunchbase.com/company/ideeli').gsub(/<div class="milestone_text">|<\/div>/,'')
+      puts text
+    end
+    
+
+
+=begin   
     text = milestone.at_css('.milestone_text').to_s().gsub(/<div class="milestone_text">|<\/div>/,'')  
     meta = milestone.at_css('.meta_milestone').to_s().gsub(/<div class="meta_milestone">|<\/div>/,'')
     puts text 
     puts meta
+=end
+
   end
 
 rescue OpenURI::HTTPError
   puts "not there"
 end
-
-
-# need to also insert crunchbase.com w/in URLs
--- crunchnews.rb
+++ crunchnews.rb
@@ -1,16 +1,30 @@
 require 'nokogiri'
 require 'open-uri'
+require 'date'
 
 begin
-  doc = Nokogiri::HTML(open('http://www.crunchbase.com/company/facebook'))
-  news = doc.css('.col3_content').each do |news|
-    links = news.css('.recently_link').to_s().gsub(/<div class="recently_link">|<div class="recently_desc">|<\/div>/,'')
-    puts links
+  doc = Nokogiri::HTML(open('http://crunchbase.com/company/facebook'))
+  techcrunch = Date.today.prev_day.strftime("%Y/%m/%d")
+  techmeme = Date.today.prev_day.strftime("%y%m%d")
+
+  news = doc.css('.recently_link').each do |news|
+    if news.to_s() =~ /#{techcrunch}|#{techmeme}/
+      links = news.to_s().gsub(/<div class="recently_link">|<\/div>/,'')
+      puts links
+    end
+
+=begin
+
+    links = news.css('.recently_link').to_s().gsub(/<div class="recently_link">|<div class="recently_desc">|<\/div>/,'')    
+    if links =~ /#{yesterday}/
+      puts links
+      puts 'asshole'
+    end
+
+=end
+
   end
 
 rescue OpenURI::HTTPError
   puts "not there"
 end
-
-
-# will need to limit it to just the techcrunch and techmeme posts
