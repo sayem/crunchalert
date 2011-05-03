4886d1b446becc606867523f7b49ead0524c3951
Sayem Islam <mail@sayemislam.com>
2011-05-03 13:41:05 -0400

    user.rb

--- crunchbase.rb
+++ crunchbase.rb
@@ -1,22 +1,29 @@
 require 'nokogiri'
 require 'open-uri'
+require 'date'
 
 begin
+  doc = Nokogiri::HTML(open('http://crunchbase.com'))
 
-# need to edit once crunchbase has some milestones
+  month = Date.today.month.to_s()
+  day = Date.today.day.to_s()
+  year = Date.today.year.to_s()
+  year.slice!(0..1)
+  today = month << '/' << day << '/' << year
 
-
-  doc = Nokogiri::HTML(open('http://www.crunchbase.com'))
   milestones = doc.css('#milestones li').each do |milestone|
-    text = milestone.at_css('.milestone_text').to_s().gsub(/<div class="milestone_text">|<\/div>/,'') 
-    meta = milestone.at_css('.meta_milestone').to_s().gsub(/<div class="meta_milestone">|<\/div>/,'')
-    puts text 
-    puts meta
+
+=begin
+    yesterday = Date.today.prev_day.strftime("%m/%d/%y")
+    yesterday.slice!(0) if yesterday.slice(0) == '0'
+=end
+
+    if milestone.text =~ /#{today}/
+      text = milestone.at_css('.milestone_text').to_s().gsub(/\/company\//,'http://crunchbase.com/company/').gsub(/\/person\//,'http://crunchbase.com/person/').gsub(/\/financial-organization\//,'http://crunchbase.com/financial-organization/').gsub(/<div class="milestone_text">|<\/div>/,'')
+      puts text
+    end
   end
 
 rescue OpenURI::HTTPError
   puts "not there"
 end
-
-
-# insert crunchbase.com into the URLs
