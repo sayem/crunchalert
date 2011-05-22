8c0734c6f126a4eb49b1cf065b59d33b641b5089
Sayem Islam <mail@sayemislam.com>
2011-05-22 02:21:51 -0400

    page layout/partials

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,21 @@
 class PagesController < ApplicationController
   def home
+    require 'date'
+    require 'htmlentities'
+    coder = HTMLEntities.new
+    yesterday = Date.today.prev_day.strftime("%a").downcase
+    news = WeeklyNews.find_by_id('1').send(yesterday).to_s.split(/\"/)
+    news.collect! {|x| coder.decode(x)}
+    news.collect! {|x| x.gsub(/--- /, '').gsub(/\n- /, '').gsub(/\\xE2\\x80\\x94/,'&mdash;')}
+    @news = news
+
+    alert_count = Alert.group('content').count
+    popular = alert_count.sort {|a,b| -1*(a[1]<=>b[1]) }
+    popular.slice!(5..-1)
+    @popular = popular
+ 
+    @recent = Alert.order('created_at').limit('10')
+
     if signed_in?
       @alerts = Alert.where(:user_id => current_user[:id])
     end
